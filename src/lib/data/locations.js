7f8b9e0bccf87bdf755496bbba19cb00ced69bf5
Steven <steven@steventan.com.au>
2022-12-24 23:23:33 +1100

    Create locations.js

---
+++ src/lib/data/locations.js
@@ -0,0 +1,69 @@
+const Locations = [
+	{
+		label: 'Dublin, Ireland',
+		latitude: 53.35014,
+		longitude: -6.266155
+	},
+
+	{
+		label: 'Geneva, Switzerland',
+		latitude: 46.204391,
+		longitude: 6.143158
+	},
+
+	{
+		label: 'Kowloon, Hong Kong',
+		latitude: 22.316668,
+		longitude: 114.183334
+	},
+
+	{
+		label: 'London, UK',
+		latitude: 51.509865,
+		longitude: -0.118092
+	},
+
+	{
+		label: 'Melbourne, Australia',
+		latitude: -37.840935,
+		longitude: 144.946457
+	},
+
+	{
+		label: 'Milan, Italy',
+		latitude: 45.464664,
+		longitude: 9.18854
+	},
+
+	{
+		label: 'Rome, Italy',
+		latitude: 41.902782,
+		longitude: 12.496366
+	},
+
+	{
+		label: 'Sydney, Australia',
+		latitude: -33.865143,
+		longitude: 151.2099
+	},
+
+	{
+		label: 'Singapore',
+		latitude: 1.29027,
+		longitude: 103.851959
+	},
+
+	{
+		label: 'Taipei City, Taiwan',
+		latitude: 25.105497,
+		longitude: 121.597366
+	},
+
+	{
+		label: 'Venice, Italy',
+		latitude: 45.438759,
+		longitude: 12.327145
+	}
+]
+
+export default Locations
